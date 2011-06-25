81e1e31d95f279f5e5cd0c0e51eaeee01a7e7628
Andreas Gohr <andi@splitbrain.org>
2011-06-26 01:05:32 +0200

    fixed CN handling in iCal parser

--- src/org/splitbrain/simpleical/SimpleIcalParser.java
+++ src/org/splitbrain/simpleical/SimpleIcalParser.java
@@ -7,12 +7,12 @@ import java.io.InputStreamReader;
 
 /**
  * A simple, optimistic approach to parsing iCal data
- * 
+ *
  * This parser doesn't handle most of the specialities of RFC2445 but is able
  * to find at least simpler values . If you need completeness use ical4j, if
  * you need something simple with a very low footprint this might be the right
  * thing for you.
- * 
+ *
  * @author Andreas Gohr <andi@splitbrain.org>
  */
 public class SimpleIcalParser {
@@ -23,16 +23,16 @@ public class SimpleIcalParser {
 
     /**
      * Initializes the parser on the given input stream
-     * 
+     *
      * @param is Stream to the iCal formatted data to parse
      */
     public SimpleIcalParser(InputStream is){
 	reader = new BufferedReader(new InputStreamReader(is));
     }
-    
+
     /**
      * Read the next VEvent from the stream
-     * 
+     *
      * @return The Event or null if no more events can be found.
      * @throws IOException when reading from the stream fails
      */
@@ -66,13 +66,13 @@ public class SimpleIcalParser {
         	String[] params = null;
         	String[] opts   = null;
         	String[] alt    = null;
-        	
+
         	int col = line.indexOf(":");
         	if(col != -1){
         	    // split into key/value pair
         	    key = line.substring(0, col);
         	    val = line.substring(col+1);
-        	    
+
         	    // key might contain parameters
         	    params = key.split("(?<!\\\\);");
         	    key = params[0]; // first part is the real key name
@@ -81,22 +81,22 @@ public class SimpleIcalParser {
         		// add all parameters as extra
         		if(opts.length == 2){
         		    event.set(key,unslash(opts[0]),unslash(opts[1]));
-        		    if(opts[0] == "CN" && val.length() == 0){
-        			val = opts[1]; // always use CN as value if not yet set
+        		    if(opts[0].equals("CN")){
+        			val = opts[1]+';'+val; // always use CN as value if not yet set
         		    }else if(frst.length() == 0){
-        			frst = opts[1]; // copy 1st one to value if not yet set
+        			frst = opts[0]; // copy 1st one to value if not yet set
         		    }
         		}else{
         		    event.set(key,unslash(opts[0]),""); // add as empty parameter
         		    if(frst.length() == 0) frst = opts[0]; // copy 1st one to value if not yet set
         		}
         	    }
-        	    
+
         	    // value might contain parameters
         	    params = val.split("(?<!\\\\)[;]");
         	    val = "";
-        	    for(int i=0; i<params.length; i++){
-        		opts = params[i].split("(?<!\\\\)[=]",2);
+        	    for (String param : params) {
+        		opts = param.split("(?<!\\\\)[=]",2);
         		if(opts.length == 2){
         		    // add parameter as extra
         		    event.set(key,unslash(opts[0]),unslash(opts[1]));
@@ -115,18 +115,18 @@ public class SimpleIcalParser {
         		    }
         		}
         	    }
-        	    
+
         	    // set value to event
         	    if(val.length() == 0) val = frst;
         	    event.set(key, unslash(val));
         	}
             }
-            
+
             line = nextline;
         }
         return null; //Shouldn't be reached except for EOF
     }
-    
+
     private String unslash(String str){
 	str = str.replaceAll("\\\\n", "\n");
 	str = str.replaceAll("\\\\(.)", "$1");
