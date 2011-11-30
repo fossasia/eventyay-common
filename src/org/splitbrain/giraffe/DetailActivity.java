9d46dbecd1211b0801d248c6bf231a4377cfc1ff
Andreas Gohr <andi@splitbrain.org>
2011-11-30 23:39:04 +0100

    some iprovements on the detail view

--- src/org/splitbrain/giraffe/DetailActivity.java
+++ src/org/splitbrain/giraffe/DetailActivity.java
@@ -18,61 +18,70 @@ public class DetailActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.detail);
-	this.context = this;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail);
+        this.context = this;
 
-	String id = "";
-	Bundle extras = getIntent().getExtras();
-	if(extras != null) id = extras.getString("id");
+        String id = "";
+        Bundle extras = getIntent().getExtras();
+        if(extras != null) id = extras.getString("id");
 
-	DBAdapter db = new DBAdapter(this);
-	db.open();
-	EventRecord record = db.getEvent(id);
-	db.close();
+        DBAdapter db = new DBAdapter(this);
+        db.open();
+        EventRecord record = db.getEvent(id);
+        db.close();
 
-	TextView tv;
-	tv = (TextView) findViewById(R.id.detail_title);
-	tv.setText(record.title);
+        TextView tv;
+        tv = (TextView) findViewById(R.id.detail_title);
+        tv.setText(record.title);
 
-	tv = (TextView) findViewById(R.id.detail_text);
-	tv.setText(record.description);
+        tv = (TextView) findViewById(R.id.detail_text);
+        tv.setText(record.description);
 
-	tv = (TextView) findViewById(R.id.detail_speaker);
-	tv.setText(record.speaker);
+        tv = (TextView) findViewById(R.id.detail_speaker);
+        if(record.speaker.length() > 0){
+            tv.setText(record.speaker);
+        }else{
+            tv.setVisibility(View.GONE);
+        }
 
-	tv = (TextView) findViewById(R.id.detail_location);
-	tv.setText(record.location);
+        tv = (TextView) findViewById(R.id.detail_location);
+        if(record.location.length() > 0){
+            tv.setText(record.location);
+        }else{
+            tv.setVisibility(View.GONE);
+        }
 
-	tv = (TextView) findViewById(R.id.detail_url);
-	if(record.url.length() > 0){
-	    tv.setTag(record.url);
-	    tv.setOnClickListener(click_url);
-	    tv.setVisibility(View.VISIBLE);
-	}else{
-	    tv.setVisibility(View.GONE);
-	}
 
-	String dt = "";
-	SimpleDateFormat df = new SimpleDateFormat("E, HH:mm");
+        tv = (TextView) findViewById(R.id.detail_url);
+        if(record.url.length() > 0){
+            tv.setTag(record.url);
+            tv.setOnClickListener(click_url);
+            tv.setVisibility(View.VISIBLE);
+        }else{
+            tv.setVisibility(View.GONE);
+        }
+
+        String dt = "";
+        SimpleDateFormat df = new SimpleDateFormat("E, MMM d HH:mm");
         dt = df.format(new Date(record.starts*1000));
         if(record.ends > 0){
             df = new SimpleDateFormat("HH:mm");
             dt += " - "+df.format(new Date(record.ends*1000));
         }
 
-	tv = (TextView) findViewById(R.id.detail_timeslot);
-	tv.setText(dt);
+        tv = (TextView) findViewById(R.id.detail_timeslot);
+        tv.setText(dt);
 
-	ImageView iv = (ImageView) findViewById(R.id.detail_favorite);
-	iv.setClickable(true);
-	if(record.favorite){
-	    iv.setImageResource(R.drawable.icon);
-	}else{
-	    iv.setImageResource(R.drawable.icongray);
-	}
-	iv.setTag(record.id);
-	iv.setOnClickListener(click_favorite);
+        ImageView iv = (ImageView) findViewById(R.id.detail_favorite);
+        iv.setClickable(true);
+        if(record.favorite){
+            iv.setImageResource(R.drawable.icon);
+        }else{
+            iv.setImageResource(R.drawable.icongray);
+        }
+        iv.setTag(record.id);
+        iv.setOnClickListener(click_favorite);
 
     }
 
@@ -82,20 +91,20 @@ public class DetailActivity extends Activity {
      * @FIXME duplicates code from EventItemAdapter
      */
     private final OnClickListener click_favorite = new OnClickListener(){
-	public void onClick(View fav) {
-	    ImageView favIV = (ImageView) fav;
+        public void onClick(View fav) {
+            ImageView favIV = (ImageView) fav;
 
-	    // toggle fav
-	    DBAdapter db = new DBAdapter(context);
-	    db.open();
-	    boolean isfav = db.toggleFavorite((String) favIV.getTag());
-	    if(isfav){
-        	favIV.setImageResource(R.drawable.icon);
+            // toggle fav
+            DBAdapter db = new DBAdapter(context);
+            db.open();
+            boolean isfav = db.toggleFavorite((String) favIV.getTag());
+            if(isfav){
+                favIV.setImageResource(R.drawable.icon);
             }else{
-        	favIV.setImageResource(R.drawable.icongray);
+                favIV.setImageResource(R.drawable.icongray);
             }
-	    db.close();
-	}
+            db.close();
+        }
     };
 
     /**
@@ -104,10 +113,10 @@ public class DetailActivity extends Activity {
      * Opens the web browser
      */
     private final OnClickListener click_url = new OnClickListener(){
-	public void onClick(View link) {
-	    TextView tv = (TextView) link;
-	    Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse((String) tv.getTag()));
-	    startActivity(i);
-	}
+        public void onClick(View link) {
+            TextView tv = (TextView) link;
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse((String) tv.getTag()));
+            startActivity(i);
+        }
     };
 }
