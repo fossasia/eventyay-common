3293b79bc910ab6390c62a5b2dddde4f18553928
Andreas Gohr <andi@splitbrain.org>
2016-09-04 16:28:46 +0200

    conversion to an Android Studio gradle based setup

---
+++ app/src/test/java/org/splitbrain/simpleical/SimpleIcalEventTest.java
@@ -0,0 +1,42 @@
+package org.splitbrain.simpleical;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class SimpleIcalEventTest {
+    @Test
+    public void testParseDate() {
+        String[] dates = {
+                "20110413T170000",
+                "20110413T170000Z",
+                "20110413T170000+03:12",
+                "20110413T170000+0312"
+        };
+
+        String fails = "";
+        for (String date : dates) {
+            try{
+                SimpleIcalEvent.parseDate(date);
+            }catch (Exception e){
+                fails += e.getMessage()+"\n";
+            }
+        }
+        if(fails.length() > 0) fail(fails);
+
+    }
+
+
+    @Test
+    public void testParseDuration1() {
+        assertEquals(1000* (15*24*60*60 + 5*60*60 + 20), SimpleIcalEvent.parseDuration("P15DT5H0M20S"));
+    }
+
+    @Test
+    public void testParseDuration2() {
+        assertEquals(1000* (60*60), SimpleIcalEvent.parseDuration("PT1H00M"));
+    }
+}
