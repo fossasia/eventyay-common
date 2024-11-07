f3e6f71be22690d5bbbec3518e3ef889b659b22d
Sak1012 <1012srivatsavas@gmail.com>
2024-11-07 08:22:08 +0530

    conf fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/eventyay-checkin/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
